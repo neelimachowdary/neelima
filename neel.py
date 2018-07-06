a0d38cca5b7cfbc89c598e493e759255d5424822
neelimachowdary <40882468+neelimachowdary@users.noreply.github.com>
2018-07-06 12:53:27 +0530

    Create neel.py

---
+++ neel.py
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char l;
+printf("enter the character");
+scanf("%c",&l);
+if(l=='a'||l=='e'||l=='i'||l=='o'||l=='u')
+{
+printf("it is vowel");
+}
+else
+{
+printf("it is not vowel");
+}
+}
