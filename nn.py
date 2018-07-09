90f8eeb99a7b2d140f9da337508a4559b0c41e9c
neelimachowdary <40882468+neelimachowdary@users.noreply.github.com>
2018-07-09 10:18:06 +0530

    Create nn.py

---
+++ nn.py
@@ -0,0 +1,15 @@
+b=23
+c=45
+d=78
+if(b>c):
+	if(b>d):
+		print("b is greater ")
+	else:
+		print("d is greater")
+elif(c>b):
+	if(c>d):
+		print("c is greater " )
+	else:
+	    print("d is greater")
+else:
+	print("d is greater")
